d20fc0e5cb292aac3fd017f8066a71da09314edf
Jose Rubem <joserubemn31@gmail.com>
2020-12-29 20:09:04 +0000

    chore: connect with database

---
+++ src/database/index.js
@@ -0,0 +1,17 @@
+const { query } = require('express')
+const { Client } = require('pg')
+
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  user: 'root',
+  password: 'root',
+  database: 'cooking'
+})
+
+client.connect()
+
+exports.query = async (query, values) => {
+  const { rows } = await client.query(query, values)
+  return rows
+}
