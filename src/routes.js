c7da359630733d6b1e31eb294458e057d3af709a
Jose Rubem <joserubemn31@gmail.com>
2020-12-29 19:45:50 +0000

    chore: configure router

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+const { Router } = require('express')
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (request, response) => {
+  response.send('Hello World')
+})
+
+module.exports = router
