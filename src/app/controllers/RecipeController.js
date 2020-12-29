cec52fa644f47ea219b5a8c2b9b73e7f2aca067e
Jose Rubem <joserubemn31@gmail.com>
2020-12-29 20:09:41 +0000

    chore: created RecipeController

---
+++ src/app/controllers/RecipeController.js
@@ -0,0 +1,23 @@
+class RecipeController {
+  index(request, response) {
+
+  }
+
+  store(request, response) {
+
+  }
+
+  show(request, response) {
+
+  }
+
+  update(request, response) {
+
+  }
+
+  delete(request, response) {
+
+  }
+}
+
+module.exports = new RecipeController()
