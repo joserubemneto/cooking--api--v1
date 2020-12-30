9e6fa22b670948579b82f2b205d301792f00271c
Jose Rubem <joserubemn31@gmail.com>
2020-12-30 21:38:12 +0000

    feat: created update method

--- src/app/controllers/RecipeController.js
+++ src/app/controllers/RecipeController.js
@@ -20,6 +20,22 @@ class RecipeController {
 
     response.json(recipe)
   }
+
+  async update(request, response) {
+    const { id } = request.params
+    const { title, ingredients, preparation, information } = request.body
+
+    const recipeExists = await RecipesRepository.findById(id)
+    if (!recipeExists) return response.status(404).json({ error: 'Recipe not exists' })
+
+    if (!title) return response.status(400).json({ error: 'Title is required' })
+    if (!ingredients) return response.status(400).json({ error: 'Ingredients is required' })
+    if (!preparation) return response.status(400).json({ error: 'Preparation is required' })
+
+    const recipe = await RecipesRepository.update(id, { title, ingredients, preparation, information })
+
+    response.json(recipe)
+  }
 }
 
 module.exports = new RecipeController()
