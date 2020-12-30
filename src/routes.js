1f729504d0897075bb6881313c2af5ee90f1d67f
Jose Rubem <joserubemn31@gmail.com>
2020-12-30 21:21:49 +0000

    chore: created recipes routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 const { Router } = require('express')
-const express = require('express')
-const router = express.Router()
 
-router.get('/', (request, response) => {
-  response.send('Hello World')
-})
+const router = Router()
+const RecipeController = require('./app/controllers/RecipeController')
+
+router.get('/recipes', RecipeController.index)
+router.post('/recipes', RecipeController.store)
+router.get('/recipe/:id', RecipeController.show)
+router.put('/recipe/:id', RecipeController.update)
+router.delete('/recipe/:id', RecipeController.delete)
 
 module.exports = router
