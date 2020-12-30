5decd04f3ed8c49c20f2d18df21a2c8dcbd8ece6
Jose Rubem <joserubemn31@gmail.com>
2020-12-30 21:36:35 +0000

    feat: created store controller

--- src/app/controllers/RecipeController.js
+++ src/app/controllers/RecipeController.js
@@ -8,6 +8,18 @@ class RecipeController {
 
     response.json(recipes)
   }
+
+  async store(request, response) {
+    const { title, ingredients, preparation, information } = request.body
+
+    if (!title) return response.status(400).json({ error: 'Title is required' })
+    if (!ingredients) return response.status(400).json({ error: 'Ingredients is required' })
+    if (!preparation) return response.status(400).json({ error: 'Preparation is required' })
+
+    const recipe = await RecipesRepository.create({ title, ingredients, preparation, information })
+
+    response.json(recipe)
+  }
 }
 
 module.exports = new RecipeController()
