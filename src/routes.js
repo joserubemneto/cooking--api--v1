5e21b04b7f86fe4522ca8752138b2d936de8533f
Jose Rubem <joserubemn31@gmail.com>
2020-12-30 21:45:26 +0000

    chore: created categories routes

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,18 @@ const { Router } = require('express')
 
 const router = Router()
 const RecipeController = require('./app/controllers/RecipeController')
+const CategoryController = require('./app/controllers/CategoryController')
 
 router.get('/recipes', RecipeController.index)
 router.post('/recipes', RecipeController.store)
 router.get('/recipe/:id', RecipeController.show)
 router.put('/recipe/:id', RecipeController.update)
 router.delete('/recipe/:id', RecipeController.delete)
+
+router.get('/categories', CategoryController.index)
+router.post('/categories', CategoryController.post)
+router.get('/category/:id', CategoryController.show)
+router.put('/category/:id', CategoryController.update)
+router.delete('/category/:id', CategoryController.delete)
 
 module.exports = router
