cc74f95cc9ee4a62add0728065d49b53c86e055a
Jose Rubem <joserubemn31@gmail.com>
2020-12-30 22:11:49 +0000

    feat: created find all method

--- src/app/repositories/CategoriesRepository.js
+++ src/app/repositories/CategoriesRepository.js
@@ -1,6 +1,15 @@
-class CategoriesRepository {
-  findAll() {
+const db = require('../../database')
 
+class CategoriesRepository {
+  async findAll(orderBy = 'ASC') {
+    const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC'
+
+    const rows = await db.query(`
+      SELECT * FROM categories
+      ORDER BY name ${direction}
+    `)
+
+    return rows
   }
 
   findByName() {
