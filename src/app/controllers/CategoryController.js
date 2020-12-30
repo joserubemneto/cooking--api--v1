009e67738d7d3240abf923b9143fa4fc811a2e0c
Jose Rubem <joserubemn31@gmail.com>
2020-12-30 22:13:41 +0000

    feat: created index method

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -1,6 +1,12 @@
-class CategoryController {
-  index() {
+const CategoriesRepository = require('../repositories/CategoriesRepository')
 
+class CategoryController {
+  async index(request, response) {
+    const { orderBy } = request.query
+
+    const categories = await CategoriesRepository.findAll(orderBy)
+
+    response.json(categories)
   }
 
   store() {
