ea279ee3907715b9e32939d578d17ced377e156e
Jose Rubem <joserubemn31@gmail.com>
2020-12-31 09:03:15 +0000

    feat: added create method

--- src/app/repositories/CategoriesRepository.js
+++ src/app/repositories/CategoriesRepository.js
@@ -12,12 +12,23 @@ class CategoriesRepository {
     return rows
   }
 
-  findByName() {
+  async findByName(name) {
+    const [row] = await db.query(`
+      SELECT * FROM categories
+      WHERE name = $1
+    `, [name])
 
+    return row
   }
 
-  create() {
+  async create({ name }) {
+    const [row] = await db.query(`
+      INSERT INTO categories (name)
+      VALUES ($1)
+      RETURNING *
+    `, [name])
 
+    return row
   }
 
   update() {
