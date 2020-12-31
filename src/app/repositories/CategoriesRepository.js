5a80e666f369e11cab946a9b4d632c38256f86f9
Jose Rubem <joserubemn31@gmail.com>
2020-12-31 09:12:57 +0000

    feat: created find by id method

--- src/app/repositories/CategoriesRepository.js
+++ src/app/repositories/CategoriesRepository.js
@@ -12,6 +12,15 @@ class CategoriesRepository {
     return rows
   }
 
+  async findById(id) {
+    const [row] = await db.query(`
+      SELECT * FROM categories
+      WHERE id = $1
+    `, [id])
+
+    return row
+  }
+
   async findByName(name) {
     const [row] = await db.query(`
       SELECT * FROM categories
