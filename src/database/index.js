ef6114fbc97965569cf31071ee74f7efcd131383
Jose Rubem <joserubemn31@gmail.com>
2021-01-02 12:13:49 +0000

    feat: added environment variables

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,15 @@
+require('dotenv').config()
+
 const { Client } = require('pg')
 
+const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB } = process.env
+
 const client = new Client({
-  host: 'localhost',
-  port: 5432,
-  user: 'root',
-  password: 'root',
-  database: 'cooking'
+  host: DB_HOST,
+  port: DB_PORT,
+  user: DB_USER,
+  password: DB_PASS,
+  database: DB,
 })
 
 client.connect()
