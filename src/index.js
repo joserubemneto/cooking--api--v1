bcd5e061ffeec3127d59707e3dfeb4b28e20bb47
Jose Rubem <joserubemn31@gmail.com>
2021-01-02 13:17:59 +0000

    feat: added error handler middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require('express')
+require('express-async-errors')
+
 const app = express()
 const routes = require('./routes')
 
 app.use(express.json())
 app.use(routes)
+app.use((error, request, response, next) => {
+  console.log(error)
+  response.sendStatus(500)
+})
 
 app.listen(3000, () => console.log('Server is running on port http://localhost:3000'))
