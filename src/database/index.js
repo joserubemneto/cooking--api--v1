7056dac41329c15c9df7ff0ca3f4177cc8916529
Jose Rubem <joserubemn31@gmail.com>
2021-01-03 02:27:43 +0000

    chore: database deploy configs

--- src/database/index.js
+++ src/database/index.js
@@ -5,12 +5,15 @@ const { Client } = require('pg')
 const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB } = process.env
 
 const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   host: DB_HOST,
   port: DB_PORT,
   user: DB_USER,
   password: DB_PASS,
   database: DB,
-  ssl: true,
 })
 
 client.connect()
