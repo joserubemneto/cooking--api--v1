a00767ce8d105c87dbdfdaf847204343d6b2b500
Jose Rubem <joserubemn31@gmail.com>
2021-01-23 01:46:18 +0000

    feat(chefs): added find chef recipes method

--- src/app/repositories/ChefsRepository.js
+++ src/app/repositories/ChefsRepository.js
@@ -7,7 +7,7 @@ class ChefsRepository {
       SELECT chefs.*, count(recipes.id) AS recipes
       FROM chefs
       LEFT JOIN recipes ON (recipes.chef_id = chefs.id)
-      GROUP BY (chefs.id, chefs.name)
+      GROUP BY (chefs.id, chefs.name, chefs.resume)
       ORDER BY name ${direction}
     `)
 
@@ -15,43 +15,68 @@ class ChefsRepository {
   }
 
   async findById(id) {
-    const [row] = await db.query(`
+    const [row] = await db.query(
+      `
       SELECT chefs.*, count(recipes.id) AS recipes
       FROM chefs
       LEFT JOIN recipes ON (recipes.chef_id = chefs.id)
       WHERE chefs.id = $1
-      GROUP BY (chefs.id, chefs.name)
-    `, [id])
+      GROUP BY (chefs.id, chefs.name, chefs.resume)
+    `,
+      [id]
+    )
 
     return row
   }
 
-  async create({ name }) {
-    const [row] = await db.query(`
-      INSERT INTO chefs (name)
-      VALUES ($1)
+  async findRecipes(id) {
+    const rows = await db.query(
+      `
+      SELECT * FROM recipes
+      WHERE chef_id = $1
+    `,
+      [id]
+    )
+
+    return rows
+  }
+
+  async create({ name, resume }) {
+    const [row] = await db.query(
+      `
+      INSERT INTO chefs (name, resume)
+      VALUES ($1, $2)
       RETURNING *
-    `, [name])
+    `,
+      [name, resume]
+    )
 
     return row
   }
 
-  async update(id, { name }) {
-    const [row] = await db.query(`
+  async update(id, { name, resume }) {
+    const [row] = await db.query(
+      `
       UPDATE chefs
-      SET name = $1
-      WHERE id = $2
+      SET name = $1,
+      resume = $2
+      WHERE id = $3
       RETURNING *
-    `, [name, id])
+    `,
+      [name, resume, id]
+    )
 
     return row
   }
 
   async delete(id) {
-    const deleteOp = await db.query(`
+    const deleteOp = await db.query(
+      `
       DELETE FROM chefs
       WHERE id = $1
-    `, [id])
+    `,
+      [id]
+    )
 
     return deleteOp
   }
