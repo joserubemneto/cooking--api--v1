f6e9069a1e6cec8154425f29b4206b0c9b2d6046
Jose Rubem <joserubemn31@gmail.com>
2021-01-24 22:23:35 +0000

    feat: added image upload

--- src/app/controllers/ChefController.js
+++ src/app/controllers/ChefController.js
@@ -1,10 +1,26 @@
 const ChefsRepository = require('../repositories/ChefsRepository')
+const FilesRepository = require('../repositories/FilesRepository')
 
 class ChefController {
   async index(request, response) {
     const { orderBy } = request.query
 
-    const chefs = await ChefsRepository.findAll(orderBy)
+    let chefs = await ChefsRepository.findAll(orderBy)
+
+    const chefsTemp = await Promise.all(
+      chefs.map(async (chef) => {
+        const file = await FilesRepository.find(chef.file_id)
+
+        return {
+          ...chef,
+          avatar_url: `${request.protocol}://${
+            request.headers.host
+          }${file.path.replace('public', '')}`,
+        }
+      })
+    )
+
+    chefs = chefsTemp
 
     response.json(chefs)
   }
@@ -12,11 +28,21 @@ class ChefController {
   async store(request, response) {
     const { name, resume } = request.body
 
+    if (!request.file)
+      return response.status(400).json({ error: 'Send at least one image' })
+
     if (!name) return response.status(400).json({ error: 'Name is required' })
+
     if (!resume)
       return response.status(400).json({ error: 'Resume is required' })
 
-    const chef = await ChefsRepository.create({ name, resume })
+    const file = await FilesRepository.create(request.file)
+
+    const chef = await ChefsRepository.create({
+      name,
+      resume,
+      file_id: file.id,
+    })
 
     response.json(chef)
   }
@@ -24,10 +50,19 @@ class ChefController {
   async show(request, response) {
     const { id } = request.params
 
-    const chef = await ChefsRepository.findById(id)
+    let chef = await ChefsRepository.findById(id)
     const recipes = await ChefsRepository.findRecipes(id)
 
     if (!chef) return response.status(404).json({ error: 'Chef not found' })
+
+    const file = await FilesRepository.find(chef.file_id)
+
+    chef = {
+      ...chef,
+      avatar_url: `${request.protocol}://${
+        request.headers.host
+      }${file.path.replace('public', '')}`,
+    }
 
     response.json({ ...chef, recipes })
   }
@@ -51,6 +86,10 @@ class ChefController {
 
   async delete(request, response) {
     const { id } = request.params
+
+    const result = await ChefsRepository.findById(id)
+
+    await FilesRepository.delete(result.file_id)
 
     await ChefsRepository.delete(id)
 
