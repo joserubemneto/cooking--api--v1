425cb76ec2922d4d6838db6cd0ebb2b9a7de4bf5
Jose Rubem <joserubemn31@gmail.com>
2021-01-25 00:37:50 +0000

    chore: update schema.sql

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -5,7 +5,8 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS chefs (
   id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
   name text NOT NULL,
-  resume text NOT NULL
+  resume text NOT NULL,
+  file_id integer
 );
 
 CREATE TABLE IF NOT EXISTS tags (
@@ -16,7 +17,8 @@ CREATE TABLE IF NOT EXISTS tags (
 
 CREATE TABLE IF NOT EXISTS categories (
   id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
-  name text NOT NULL
+  name text NOT NULL,
+  file_id integer
 );
 
 CREATE TABLE IF NOT EXISTS files (
@@ -34,6 +36,7 @@ CREATE TABLE IF NOT EXISTS recipes (
   category_id UUID,
   chef_id UUID,
   tag_id text[],
+  file_id integer,
   FOREIGN KEY(category_id) REFERENCES categories(id),
   FOREIGN KEY(chef_id) REFERENCES chefs(id)
 );
