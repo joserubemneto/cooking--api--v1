248bacc555fe4c2cca4f8047e5772baae1d1d98b
Jose Rubem <joserubemn31@gmail.com>
2021-01-30 19:05:12 +0000

    feat: added tag resume

--- src/app/controllers/TagController.js
+++ src/app/controllers/TagController.js
@@ -17,20 +17,22 @@ class TagController {
   }
 
   async store(request, response) {
-    const { name } = request.body
+    const { name, resume } = request.body
     const recipes = []
     if (!name) return response.status(400).json({ error: 'Name is required' })
+    if (!resume)
+      return response.status(400).json({ error: 'Resume is required' })
 
-    const tag = await TagsRepository.create(name, recipes)
+    const tag = await TagsRepository.create(name, resume, recipes)
 
     response.json(tag)
   }
 
   async update(request, response) {
     const { id } = request.params
-    const { name, recipes } = request.body
+    const { name, resume, recipes } = request.body
 
-    const tag = await TagsRepository.update(id, { name, recipes })
+    const tag = await TagsRepository.update(id, { name, resume, recipes })
 
     response.json(tag)
   }
